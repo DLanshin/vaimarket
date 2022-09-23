1f72acfc3a72af52b0d8e28f389c2d6156180760
DaniilLanshinSPB <d.lanshin@finlead.team>
2022-09-23 15:48:58 +0300

    fast fix

--- js/main.js
+++ js/main.js
@@ -18,8 +18,13 @@ function init() {
     initMobileMenu();
     initHeaderSearch();
     initProductSlider();
+    initPriceSlider();
+    initMultipleSelect();
+    initCategorySlider();
+    initConsultationButton();
     $(window).resize(function () {
         initShopSlider();
+        initCategorySlider();
         initProductSlider(true);
     })
 }
@@ -74,7 +79,7 @@ function initShopSlider() {
             $wrapper.find('.shops-slider__item').each(function (index, item) {
                 if (index % 2 == 0) {
                     var current = $(item);
-                    var next = $(item).next();
+                    var next = current.next().hasClass("cards-category__item") ? current.next() :"";
                     $wrapper.append($('<div>').addClass("shops__slide").append(current, next));
                 }
             });
@@ -109,7 +114,48 @@ function initShopSlider() {
 
 
 }
+/*CATEGORY SLIDER*/
+function initCategorySlider() {
+    var $wrapper = $('#category-slider');
+    if (window.innerWidth < 768) {
+        if (!$wrapper.find(".cards-category__slide").length) {
+            $wrapper.find('.cards-category__item').each(function (index, item) {
+                if (index % 2 == 0) {
+                    var current = $(item);
+                    var next = current.next().hasClass("cards-category__item") ? current.next() :"";
+                    $wrapper.append($('<div>').addClass("cards-category__slide").append(current, next));
+                }
+            });
+            $wrapper.slick({
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                lazyLoad: "progressive",
+                responsive: [
+                    {
+                        breakpoint: 520,
+                        settings: {
+                            slidesToShow: 2,
+                        }
+                    }
+                ]
+            });
+            $wrapper.css("opacity", 1);
+        }
+    } else {
+        if ($wrapper.find(".cards-category__slide").length) {
+            $wrapper.slick('unslick');
+            $wrapper.find('.cards-category__item').each(function (index, item) {
+                $wrapper.append($(item));
+            });
+            $wrapper.find(".cards-category__slide").remove()
+        }
+    }
 
+
+}
 /*SHOW PAGE*/
 function initProductSlider(isResized = false) {
     let $slider = $(".js-product-slider");
@@ -147,11 +193,71 @@ function initProductSlider(isResized = false) {
     }
 
     $(".js-show-more-products").on("click", function(){
-        console.log("open");
         $slider.find('.product-slider__item.hidden').each(function (index, item) {
             if(index < 4){
                 $(item).removeClass("hidden");
             }
         });
     });
+}
+
+function initPriceSlider(){
+    let $priceRange = $(".js-price-range");
+    let $priceSlider = $(document).find(".js-price-slider");
+    let min = $('.js-price-slider').data("min");
+    let max = $('.js-price-slider').data("max");
+    let from = $('.js-price-slider').data("from");
+    let to = $('.js-price-slider').data("to");
+    $priceSlider.slider({
+        range: true,
+        min: min,
+        max: max,
+        values: [ from, to ],
+        slide: function( event, ui ) {
+            $priceRange.find("input[name='minPrice']").val(ui.values[0]);
+            $priceRange.find("input[name='maxPrice']").val(ui.values[1]);
+        }
+    });
+    $priceRange.find("input[name='minPrice']").val($priceSlider.slider( "values", 0 ));
+    $priceRange.find("input[name='maxPrice']").val($priceSlider.slider( "values", 1 ));
+}
+
+function initMultipleSelect(){
+    let $select = $(".js-multiple");
+    let selectListHtml = "";
+    $select.find("option").each(function(i, item){
+        selectListHtml += ''+
+            '<li class="select-multiple__item">'+
+            '    <div class="select-multiple__item-checkbox checkbox">'+
+            '        <input type="checkbox" class="checkbox__input" id="item-'+$(item).prop("value")+'" name="'+$(item).text()+'" value="'+$(item).prop("value")+'">'+
+            '        <label for="item-'+$(item).prop("value")+'">'+$(item).text()+'</label>'+
+            '    </div>'+
+            '</li>';
+    });
+    $select.after($('<div class="select-multiple"></div>')
+            .append('<div class="select-multiple__selection"><input class="select-multiple__selection-search-field" placeholder="Поиск магазина"></div>')
+            .append($('<ul class="select-multiple__list"></ul>').append('<div class="select-multiple__items"></div>').html(selectListHtml))
+
+    );
+    $select.hide();
+    /*live search*/
+    $(document).on("keyup", ".select-multiple__selection-search-field", function(){
+        let findText = $(this).val().toLowerCase();
+        let $list = $(this).closest(".select-multiple").find(".select-multiple__list");
+        console.log("search")
+        $.each($list.find("li label"), function() {
+            if($(this).text().toLowerCase().indexOf(findText) === -1) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        });
+    });
+}
+
+function initConsultationButton(){
+    $(document).on("click", ".js-open-consultation-message", function(){
+        console.log("open")
+        $(this).closest(".consultation").find(".consultation__card-box").toggleClass("open");
+    })
 }
